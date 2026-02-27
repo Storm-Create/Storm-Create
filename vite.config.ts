915f9f326e75f65000c7013765de904dd333f5a9
Fertyyy <divfurhfyg@gmail.com>
2026-02-28 02:19:15 +0300

    Add tariffs and docs to Vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -31,6 +31,7 @@ export default defineConfig(({ mode }) => {
           blog: path.resolve(__dirname, 'blog.html'),
           post: path.resolve(__dirname, 'post.html'),
           tariffs: path.resolve(__dirname, 'tariffs.html'),
+          docs: path.resolve(__dirname, 'docs.html'),
         },
       },
     },
